745e305a0001ae039bdfabd03db02767996ebd18
Ryan Laddusaw <rladdusaw@princeton.edu>
2024-04-12 16:07:47 -0400

    Parsing the marc file and checking 001 fields
    Co-authored-by: Beck Davis <beck-davis@users.noreply.github.com>
    Co-authored-by: Christina Chortaria <christinach@users.noreply.github.com>
    Co-authored-by: Jane Sandberg <sandbergja@users.noreply.github.com>
    Co-authored-by: Kevin Reiss <kevinreiss@users.noreply.github.com>
    Co-authored-by: Max Kadel <maxkadel@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,29 @@
-use std::fs::File;
+use std::{fs::File};
+
+use marc::{Records, Record};
 
 fn main() {
-  let _input = File::open("tests/fixtures/marc_data.mrc").unwrap();
+  let input = File::open("tests/fixtures/marc_data.mrc").unwrap();
+  let mut count = 0;
+
+  for (i, record) in Records::new(input).enumerate() {
+    let record = record.unwrap();
+    match i {
+      0 => assert_eq!(record.field(b"001")[0].get_data::<str>(), "020598225"),
+      1 => assert_eq!(record.field(b"001")[0].get_data::<str>(), "021175603"),
+      2 => assert_eq!(record.field(b"001")[0].get_data::<str>(), "021641563"),
+      _ => println!("ID: {}", get_id(record)),
+    }
+    count += 1;
+  }
+
+  assert_eq!(count, 536);
+}
+
+fn get_id(record: Record) -> String {
+  if record.field(b"001").is_empty() {
+    String::from("No ID")
+  } else {
+    String::from(record.field(b"001")[0].get_data::<str>())
+  }
 }
